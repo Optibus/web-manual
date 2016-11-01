3e273c803fa4067a5f38f387dd71f3980d2e4216
Flare project <team@optibus.co>
2016-11-01 17:51:29 +0200

    publish

---
+++ Data/Glossary.js
@@ -0,0 +1,1 @@
+define({numchunks:1,prefix:'Glossary_Chunk',terms:[{t:'Changeover',c:0},{t:'Dataset',c:0},{t:'Deadhead',c:0},{t:'Duty',c:0},{t:'Post trip',c:0},{t:'Pre trip',c:0},{t:'Pull in',c:0},{t:'Pull out',c:0},{t:'Revision',c:0},{t:'Roster',c:0},{t:'Schedule',c:0},{t:'Split',c:0},{t:'Trip',c:0},{t:'Vehicle',c:0}]});
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=636135977356495979'
+});
